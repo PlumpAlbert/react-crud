c945ff8982c1b15233d14e983e61262de7620e39
plump_albert <plumpalbert@gmail.com>
2019-06-18 20:29:31 +0300

    Updated Home component

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -5,11 +5,26 @@ const Home = props => {
   console.log(props);
   return (
     <div className="home">
-      <h1 className='product-header'>Welcome to The Store&trade;</h1>
-      <p className="product-description">
-        This is an awesome description for this store.
-      </p>
-      <button>Click me</button>
+      <div className="product">
+        <h1 className='product-header'>
+          Welcome to{' '}
+          <span className="product-name">Green Store</span>
+          <sup> &trade;</sup>
+        </h1>
+        <div className="content">
+          <p className="product-description">
+            It doesn't matter if you're a vegan or paleo or omnivore. Maybe you like kale or pizza or mochi, or maybe
+            you
+            value local. That's cool. No matter what you're into, <span className="product-name">Green Store</span> is
+            the
+            place to find the tastiest, best version of it. Whatever it is.
+          </p>
+          <img
+            src="https://images.vexels.com/media/users/3/127670/isolated/preview/1c400fa105ae69ed69e526f8a4a96a76-flat-flower-plant-tub-by-vexels.png"
+            alt="" className="product-logo"/>
+        </div>
+        <button className='btn'>GO SHOPPING!</button>
+      </div>
     </div>
   );
 };
