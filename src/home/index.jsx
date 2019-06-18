cc948c85cad7f36d7c4a15e26e8ceb2511d32586
plump_albert <plumpalbert@gmail.com>
2019-06-18 21:34:50 +0300

    Added footer

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -5,26 +5,26 @@ const Home = props => {
   console.log(props);
   return (
     <div className="home">
-      <div className="product">
-        <h1 className='product-header'>
-          Welcome to{' '}
-          <span className="product-name">Green Store</span>
-          <sup> &trade;</sup>
-        </h1>
-        <div className="content">
-          <p className="product-description">
-            It doesn't matter if you're a vegan or paleo or omnivore. Maybe you like kale or pizza or mochi, or maybe
-            you
-            value local. That's cool. No matter what you're into, <span className="product-name">Green Store</span> is
-            the
-            place to find the tastiest, best version of it. Whatever it is.
-          </p>
-          <img
-            src="https://images.vexels.com/media/users/3/127670/isolated/preview/1c400fa105ae69ed69e526f8a4a96a76-flat-flower-plant-tub-by-vexels.png"
-            alt="" className="product-logo"/>
-        </div>
-        <button className='btn'>GO SHOPPING!</button>
+      <h1 className='product-header'>
+        Welcome to{' '}
+        <span className="product-name">Green Store</span>
+      </h1>
+      <div className="content">
+        <p className="product-description">
+          It doesn't matter if you're a vegan or paleo or omnivore. Maybe you like kale or pizza or mochi, or maybe
+          you
+          value local. That's cool. No matter what you're into, <span className="product-name">Green Store</span> is
+          the
+          place to find the tastiest, best version of it. Whatever it is.
+        </p>
+        <img
+          src="https://images.vexels.com/media/users/3/127670/isolated/preview/1c400fa105ae69ed69e526f8a4a96a76-flat-flower-plant-tub-by-vexels.png"
+          alt="" className="product-logo"/>
       </div>
+      <button className='btn'>GO SHOPPING!</button>
+      <footer>
+        All rights reserved. Copyrighted by Green Store<sup>&trade;</sup>
+      </footer>
     </div>
   );
 };
