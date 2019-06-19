e2d38fa06d6d6d8e82b1d1fd2e1fc2e571f964b6
plump_albert <plumpalbert@gmail.com>
2019-06-19 03:41:12 +0300

    Added CRUD component to the App

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Menu from "../menu";
 import Home from "../home";
-import Orders from "../orders";
-import Customers from "../customers";
-import "./App.sass";
+import CRUD from "../crud";
+// import Orders from "../orders";
+// import Customers from "../customers";
+import "./app.sass";
 
 /**
  * The main component.
@@ -16,8 +17,14 @@ const App = () => (
       <Menu />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/orders" component={Orders} />
-        <Route path="/customers" component={Customers} />
+        <Route
+          path="/orders"
+          render={() => <CRUD title="Orders" items={[]} />}
+        />
+        <Route
+          path="/customers"
+          render={() => <CRUD title="Customers" items={[]} />}
+        />
       </Switch>
     </div>
   </BrowserRouter>
