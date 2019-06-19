1c2af96c6608c0f992c038a86629a20461ce6e4b
plump_albert <plumpalbert@gmail.com>
2019-06-19 13:18:20 +0300

    Fixed styling of a landing page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import "./home.sass";
+import * as React from 'react';
+import './home.sass';
 
 /**
  * The Home component.
@@ -7,23 +7,26 @@ import "./home.sass";
  */
 const Home = () => (
   <div className="home">
-    <h1 className="product-header">
-      Welcome to <span className="product-name">Green Store</span>
-    </h1>
-    <div className="content">
-      <p className="product-description">
-        It doesn't matter if you're a vegan or paleo or omnivore. Maybe you like
-        kale or pizza or mochi, or maybe you value local. That's cool. No matter
-        what you're into, <span className="product-name">Green Store</span> is
-        the place to find the tastiest, best version of it. Whatever it is.
-      </p>
-      <img
-        src="https://images.vexels.com/media/users/3/127670/isolated/preview/1c400fa105ae69ed69e526f8a4a96a76-flat-flower-plant-tub-by-vexels.png"
-        alt=""
-        className="product-logo"
-      />
-    </div>
-    <button className="btn">GO SHOPPING!</button>
+    <main>
+      <h1 className="product-header">
+        Welcome to <span className="product-name">Green Store</span>
+      </h1>
+      <div className="content">
+        <p className="product-description">
+          It doesn't matter if you're a vegan or paleo or omnivore. Maybe you
+          like kale or pizza or mochi, or maybe you value local. That's cool. No
+          matter what you're into,{' '}
+          <span className="product-name">Green Store</span> is the place to find
+          the tastiest, best version of it. Whatever it is.
+        </p>
+        <img
+          src="https://images.vexels.com/media/users/3/127670/isolated/preview/1c400fa105ae69ed69e526f8a4a96a76-flat-flower-plant-tub-by-vexels.png"
+          alt=""
+          className="product-logo"
+        />
+      </div>
+      <button className="btn">GO SHOPPING!</button>
+    </main>
     <footer>
       All rights reserved. Copyrighted by Green Store<sup>&trade;</sup>
     </footer>
