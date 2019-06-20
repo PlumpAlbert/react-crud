a18d28948cede3c6d8f2554569c0b4d19e4b4faf
plump_albert <plumpalbert@gmail.com>
2019-06-20 15:30:06 +0300

    Added generated data

---
+++ src/components/app/customers.js
@@ -0,0 +1,402 @@
+export default [
+  {
+    title: "Jaquelyn K. Simpson",
+    info: "Mattis Velit Company"
+  },
+  {
+    title: "Genevieve X. Roberson",
+    info: "Sed LLC"
+  },
+  {
+    title: "Samuel Pickett",
+    info: "Vitae Corp."
+  },
+  {
+    title: "Merritt Grant",
+    info: "Fames Ac Corp."
+  },
+  {
+    title: "Griffith Gray",
+    info: "Curabitur Industries"
+  },
+  {
+    title: "Jordan P. Riley",
+    info: "Eget Metus LLP"
+  },
+  {
+    title: "Gavin Rowe",
+    info: "Ut Quam Foundation"
+  },
+  {
+    title: "May Conrad",
+    info: "Posuere Foundation"
+  },
+  {
+    title: "Alfonso V. Sloan",
+    info: "Risus Corp."
+  },
+  {
+    title: "Nichole Lucas",
+    info: "In Inc."
+  },
+  {
+    title: "Leslie Holder",
+    info: "Tellus LLC"
+  },
+  {
+    title: "Jenette D. Lewis",
+    info: "Torquent Industries"
+  },
+  {
+    title: "Vivian F. Robinson",
+    info: "Cursus Purus Nullam Company"
+  },
+  {
+    title: "Brett Sampson",
+    info: "Scelerisque Neque Sed Ltd"
+  },
+  {
+    title: "Carla Conley",
+    info: "Fusce Industries"
+  },
+  {
+    title: "Bruce Montgomery",
+    info: "Volutpat Ornare Facilisis LLC"
+  },
+  {
+    title: "Whoopi Roy",
+    info: "Nec Ligula Institute"
+  },
+  {
+    title: "Jessica L. Huber",
+    info: "Tellus PC"
+  },
+  {
+    title: "Isabella Morse",
+    info: "Tristique Industries"
+  },
+  {
+    title: "Ima Cote",
+    info: "Facilisis Industries"
+  },
+  {
+    title: "Tatiana J. Fields",
+    info: "Amet Industries"
+  },
+  {
+    title: "Tallulah T. Mack",
+    info: "Lectus A Company"
+  },
+  {
+    title: "Kylan Q. Harper",
+    info: "Dapibus Gravida Aliquam Industries"
+  },
+  {
+    title: "Forrest R. Harris",
+    info: "Neque PC"
+  },
+  {
+    title: "Alice A. Bernard",
+    info: "Bibendum Ullamcorper Duis Limited"
+  },
+  {
+    title: "Blythe Quinn",
+    info: "Amet Risus Donec Incorporated"
+  },
+  {
+    title: "Donovan X. Higgins",
+    info: "Duis Risus Industries"
+  },
+  {
+    title: "Emma Montoya",
+    info: "Faucibus Id Libero Consulting"
+  },
+  {
+    title: "Louis O. Knowles",
+    info: "Vulputate Mauris Sagittis Corporation"
+  },
+  {
+    title: "Heidi Castaneda",
+    info: "Ornare PC"
+  },
+  {
+    title: "Otto Washington",
+    info: "Ultrices Vivamus Consulting"
+  },
+  {
+    title: "Rudyard Clay",
+    info: "Neque Nullam Ut Foundation"
+  },
+  {
+    title: "Camden G. Atkins",
+    info: "Magna Tellus Faucibus PC"
+  },
+  {
+    title: "Mason X. Jacobson",
+    info: "Etiam Ligula Incorporated"
+  },
+  {
+    title: "Derek Salas",
+    info: "Dui Foundation"
+  },
+  {
+    title: "Todd L. Mercer",
+    info: "Quam A Felis Corporation"
+  },
+  {
+    title: "Randall H. Cameron",
+    info: "Facilisis Non Foundation"
+  },
+  {
+    title: "Caldwell Villarreal",
+    info: "Et PC"
+  },
+  {
+    title: "Ryan N. Young",
+    info: "Suspendisse Aliquet Sem Corp."
+  },
+  {
+    title: "Azalia Cortez",
+    info: "Sit Amet Luctus Institute"
+  },
+  {
+    title: "Kiara Travis",
+    info: "Quisque Porttitor Eros Consulting"
+  },
+  {
+    title: "Whoopi B. Yang",
+    info: "Arcu Sed Et Company"
+  },
+  {
+    title: "Hiroko U. Wiley",
+    info: "Sit Amet Diam Limited"
+  },
+  {
+    title: "Zoe Rowe",
+    info: "Nibh Phasellus Nulla Industries"
+  },
+  {
+    title: "Brent N. Lawson",
+    info: "Lobortis PC"
+  },
+  {
+    title: "Kiara X. Stark",
+    info: "Convallis Inc."
+  },
+  {
+    title: "Nasim Holden",
+    info: "A Foundation"
+  },
+  {
+    title: "Galvin Pace",
+    info: "Purus Mauris Incorporated"
+  },
+  {
+    title: "Macon S. Hobbs",
+    info: "Eu Odio Inc."
+  },
+  {
+    title: "Declan J. Rivers",
+    info: "Dui Cras Pellentesque LLP"
+  },
+  {
+    title: "Felicia Hinton",
+    info: "Arcu Vestibulum Ante LLC"
+  },
+  {
+    title: "Aquila D. Mathis",
+    info: "Arcu Aliquam Corp."
+  },
+  {
+    title: "Tyler F. Kirby",
+    info: "Lacus Corp."
+  },
+  {
+    title: "Katell Roy",
+    info: "At Corp."
+  },
+  {
+    title: "Amery O. Pollard",
+    info: "Quisque Ornare Tortor Institute"
+  },
+  {
+    title: "Tatyana P. Pitts",
+    info: "Convallis In Cursus Incorporated"
+  },
+  {
+    title: "Hedda Donaldson",
+    info: "Faucibus Ut Limited"
+  },
+  {
+    title: "Ora Young",
+    info: "Turpis Nulla Company"
+  },
+  {
+    title: "September H. Knox",
+    info: "Lorem Tristique Incorporated"
+  },
+  {
+    title: "Brett Huff",
+    info: "Et Ultrices LLP"
+  },
+  {
+    title: "Brooke Warren",
+    info: "Vel Arcu Curabitur Company"
+  },
+  {
+    title: "India F. May",
+    info: "A Mi Fringilla Corp."
+  },
+  {
+    title: "Shafira Herrera",
+    info: "Molestie In Foundation"
+  },
+  {
+    title: "Daphne Carney",
+    info: "Ridiculus Associates"
+  },
+  {
+    title: "Maggie K. Marshall",
+    info: "Ut Associates"
+  },
+  {
+    title: "Emerald T. Ellis",
+    info: "Nulla Dignissim Incorporated"
+  },
+  {
+    title: "Jaquelyn L. Sparks",
+    info: "Vitae Odio Sagittis Associates"
+  },
+  {
+    title: "Merritt Ford",
+    info: "Consectetuer Corporation"
+  },
+  {
+    title: "Jorden Cline",
+    info: "Dui Semper Industries"
+  },
+  {
+    title: "Solomon Wood",
+    info: "In Lorem Donec Incorporated"
+  },
+  {
+    title: "Carl B. Chan",
+    info: "Quis Diam Luctus Corp."
+  },
+  {
+    title: "Mikayla Michael",
+    info: "Lorem Donec LLP"
+  },
+  {
+    title: "Aretha I. Wallace",
+    info: "Quisque Fringilla Ltd"
+  },
+  {
+    title: "Kuame Michael",
+    info: "Semper Consulting"
+  },
+  {
+    title: "Price G. Park",
+    info: "Pede Incorporated"
+  },
+  {
+    title: "Rhea Young",
+    info: "Erat Eget Incorporated"
+  },
+  {
+    title: "Barrett W. Frost",
+    info: "Purus Associates"
+  },
+  {
+    title: "Preston Moran",
+    info: "Non Dapibus Rutrum Foundation"
+  },
+  {
+    title: "Cecilia Blanchard",
+    info: "Ipsum Associates"
+  },
+  {
+    title: "Asher Ellis",
+    info: "Pede Limited"
+  },
+  {
+    title: "Sean Roy",
+    info: "A Felis LLC"
+  },
+  {
+    title: "Jarrod Walker",
+    info: "Auctor Odio LLP"
+  },
+  {
+    title: "Myra Horton",
+    info: "Vestibulum Mauris PC"
+  },
+  {
+    title: "Marsden Levy",
+    info: "Dis Industries"
+  },
+  {
+    title: "Karly Johnson",
+    info: "Dictum Placerat Augue LLP"
+  },
+  {
+    title: "Hedda X. Blankenship",
+    info: "Eu Placerat Eget LLC"
+  },
+  {
+    title: "Kerry Tyson",
+    info: "Mauris Ut Mi Corp."
+  },
+  {
+    title: "Yuli R. Hooper",
+    info: "Nunc Est Incorporated"
+  },
+  {
+    title: "Maxwell S. Carson",
+    info: "Pede Nunc LLP"
+  },
+  {
+    title: "Tamara Newman",
+    info: "Sit Amet LLC"
+  },
+  {
+    title: "Judah T. Howell",
+    info: "Varius Et Limited"
+  },
+  {
+    title: "Francis Q. Bender",
+    info: "Sed LLC"
+  },
+  {
+    title: "Karly Ellison",
+    info: "Tellus Industries"
+  },
+  {
+    title: "Jessica X. Cooley",
+    info: "Amet Incorporated"
+  },
+  {
+    title: "Gil Hayden",
+    info: "Id Magna Corp."
+  },
+  {
+    title: "Jerry Webster",
+    info: "Aliquam Nisl LLP"
+  },
+  {
+    title: "Hedley Gill",
+    info: "Lorem Sit Industries"
+  },
+  {
+    title: "Iliana A. Brooks",
+    info: "Convallis Convallis Inc."
+  },
+  {
+    title: "Linus Cooke",
+    info: "Augue Eu Tellus Associates"
+  },
+  {
+    title: "Darryl Vinson",
+    info: "Massa PC"
+  }
+];
--- src/components/app/orders.js
+++ src/components/app/orders.js
@@ -1,6 +1,409 @@
-export default Array(15)
-  .fill(0)
-  .map((v, i) => ({
-    title: `Item #${i + 1}`,
-    info: `Some great description of this item`
-  }));
+// export default Array(15)
+//   .fill(0)
+//   .map((v, i) => ({
+//     title: `Item #${i + 1}`,
+//     info: `Some great description of this item`
+//   }));
+
+export default [
+  {
+    title: "Ipsum Sodales Purus Associates",
+    info: "926-9406 Laoreet Av."
+  },
+  {
+    title: "Parturient Montes Nascetur Associates",
+    info: "6858 Libero Rd."
+  },
+  {
+    title: "Et Euismod Incorporated",
+    info: "777-2054 Vitae St."
+  },
+  {
+    title: "Sem Semper Erat Industries",
+    info: "Ap #738-2193 Malesuada Av."
+  },
+  {
+    title: "Massa Non Ante LLP",
+    info: "9712 Sagittis St."
+  },
+  {
+    title: "Mi Tempor PC",
+    info: "8206 Phasellus St."
+  },
+  {
+    title: "Egestas Urna LLC",
+    info: "448-5862 Quam Rd."
+  },
+  {
+    title: "Enim Nisl Elementum Foundation",
+    info: "761-2053 Tristique Ave"
+  },
+  {
+    title: "Aliquam Tincidunt Nunc Inc.",
+    info: "4820 Augue Road"
+  },
+  {
+    title: "Enim Diam Vel Inc.",
+    info: "Ap #913-6842 Ac Street"
+  },
+  {
+    title: "Amet Luctus Vulputate Corp.",
+    info: "811-3981 Nulla. St."
+  },
+  {
+    title: "Magna Cras Convallis Associates",
+    info: "Ap #810-9172 Eget Street"
+  },
+  {
+    title: "Mauris Sapien Cursus LLC",
+    info: "P.O. Box 417, 5458 Ullamcorper. Avenue"
+  },
+  {
+    title: "Eu Ligula Aenean Associates",
+    info: "7310 Rhoncus. Avenue"
+  },
+  {
+    title: "Ante LLP",
+    info: "P.O. Box 238, 591 Pede. Ave"
+  },
+  {
+    title: "Amet Industries",
+    info: "Ap #349-1326 In Rd."
+  },
+  {
+    title: "Tristique Inc.",
+    info: "P.O. Box 792, 7777 Dignissim Av."
+  },
+  {
+    title: "Arcu Vivamus Sit Consulting",
+    info: "P.O. Box 233, 9518 Ullamcorper Road"
+  },
+  {
+    title: "Sodales Inc.",
+    info: "P.O. Box 908, 1843 Feugiat. Rd."
+  },
+  {
+    title: "Placerat Orci LLP",
+    info: "3407 Non, Rd."
+  },
+  {
+    title: "Orci Consulting",
+    info: "Ap #722-6349 Metus Av."
+  },
+  {
+    title: "Vitae Purus Gravida Associates",
+    info: "P.O. Box 737, 3131 Auctor St."
+  },
+  {
+    title: "Volutpat Nulla Consulting",
+    info: "Ap #125-8190 Molestie Rd."
+  },
+  {
+    title: "Lorem Donec Incorporated",
+    info: "P.O. Box 143, 2082 Consequat Rd."
+  },
+  {
+    title: "Euismod Ac Incorporated",
+    info: "P.O. Box 854, 3353 Risus. Avenue"
+  },
+  {
+    title: "Ad Litora Corporation",
+    info: "Ap #161-7239 Nisi Avenue"
+  },
+  {
+    title: "Quisque Ornare PC",
+    info: "7474 Aliquet Ave"
+  },
+  {
+    title: "Magnis Ltd",
+    info: "Ap #249-8741 Egestas. Rd."
+  },
+  {
+    title: "Nisi Limited",
+    info: "3527 A Av."
+  },
+  {
+    title: "Duis Associates",
+    info: "P.O. Box 592, 1984 Tempus Av."
+  },
+  {
+    title: "Ac Arcu Industries",
+    info: "Ap #104-6938 Eu Av."
+  },
+  {
+    title: "Vel Vulputate Corp.",
+    info: "Ap #486-3715 Quam. Ave"
+  },
+  {
+    title: "Ac Turpis Egestas Limited",
+    info: "5756 Ullamcorper, Street"
+  },
+  {
+    title: "Vulputate Risus PC",
+    info: "P.O. Box 776, 2097 Rhoncus. Rd."
+  },
+  {
+    title: "Donec Consulting",
+    info: "322-1192 Nunc Road"
+  },
+  {
+    title: "Magna Company",
+    info: "Ap #598-6494 Adipiscing Ave"
+  },
+  {
+    title: "Iaculis Aliquet Diam Incorporated",
+    info: "997-2036 Et Ave"
+  },
+  {
+    title: "Ut Tincidunt Vehicula Corporation",
+    info: "Ap #652-8162 Pede St."
+  },
+  {
+    title: "Dui In Sodales Ltd",
+    info: "6556 Nec Road"
+  },
+  {
+    title: "Phasellus Libero Mauris Institute",
+    info: "1324 Urna. Avenue"
+  },
+  {
+    title: "Aliquet Proin LLC",
+    info: "Ap #788-4637 Sapien. Street"
+  },
+  {
+    title: "Nunc LLC",
+    info: "Ap #139-8679 Vestibulum Av."
+  },
+  {
+    title: "Vel Lectus Consulting",
+    info: "P.O. Box 358, 4896 Enim St."
+  },
+  {
+    title: "Non Dapibus Rutrum LLC",
+    info: "4415 Rutrum Road"
+  },
+  {
+    title: "Quis Diam Pellentesque Incorporated",
+    info: "1567 Suscipit Avenue"
+  },
+  {
+    title: "Sagittis Lobortis Corporation",
+    info: "Ap #660-6612 Mauris Rd."
+  },
+  {
+    title: "Tellus Faucibus Consulting",
+    info: "7704 Vestibulum St."
+  },
+  {
+    title: "Odio A Purus Ltd",
+    info: "Ap #972-7278 Libero. Av."
+  },
+  {
+    title: "Ac Metus Vitae Incorporated",
+    info: "5794 Fringilla Road"
+  },
+  {
+    title: "Vehicula Risus Nulla Incorporated",
+    info: "Ap #100-1768 Eu Avenue"
+  },
+  {
+    title: "Vulputate Lacus Cras Consulting",
+    info: "Ap #523-9800 In Rd."
+  },
+  {
+    title: "Vestibulum Neque Ltd",
+    info: "Ap #831-1608 Dolor. Street"
+  },
+  {
+    title: "Duis Mi Enim Institute",
+    info: "Ap #781-3504 Praesent Road"
+  },
+  {
+    title: "Aliquam PC",
+    info: "816-9434 Vivamus Av."
+  },
+  {
+    title: "Velit Eget Laoreet Inc.",
+    info: "Ap #877-5585 Magna. St."
+  },
+  {
+    title: "Tristique Neque Inc.",
+    info: "Ap #686-4556 In Avenue"
+  },
+  {
+    title: "Lorem Eget Mollis LLC",
+    info: "P.O. Box 487, 4811 Nec, St."
+  },
+  {
+    title: "Aliquet Corp.",
+    info: "127-919 Id, St."
+  },
+  {
+    title: "Elit Aliquam LLC",
+    info: "802-6636 Magna. Rd."
+  },
+  {
+    title: "Et Magnis Dis Company",
+    info: "179-8085 Nam Rd."
+  },
+  {
+    title: "Est Mauris Rhoncus PC",
+    info: "8348 Pede. St."
+  },
+  {
+    title: "In Hendrerit Inc.",
+    info: "1890 Proin Road"
+  },
+  {
+    title: "Ante Bibendum Ullamcorper PC",
+    info: "Ap #511-4786 Pede. Av."
+  },
+  {
+    title: "Quam Pellentesque Associates",
+    info: "P.O. Box 954, 9482 Dictum. St."
+  },
+  {
+    title: "Ut Semper Pretium Incorporated",
+    info: "6765 Et, Road"
+  },
+  {
+    title: "Vitae Nibh Donec LLP",
+    info: "3724 Iaculis, Avenue"
+  },
+  {
+    title: "Urna Nullam LLC",
+    info: "Ap #320-2963 Accumsan Av."
+  },
+  {
+    title: "A Corporation",
+    info: "P.O. Box 871, 8990 Non Av."
+  },
+  {
+    title: "Tincidunt Adipiscing Mauris Corporation",
+    info: "4306 Mollis Avenue"
+  },
+  {
+    title: "Et Netus Et PC",
+    info: "Ap #823-9295 Enim Road"
+  },
+  {
+    title: "Velit Associates",
+    info: "P.O. Box 872, 7271 Ultrices. St."
+  },
+  {
+    title: "Nostra Foundation",
+    info: "P.O. Box 774, 7009 Convallis St."
+  },
+  {
+    title: "Adipiscing Lacus Ut Ltd",
+    info: "Ap #664-4053 Phasellus St."
+  },
+  {
+    title: "Sodales Purus LLP",
+    info: "Ap #974-1587 Vulputate St."
+  },
+  {
+    title: "Rutrum Lorem LLP",
+    info: "358-5527 Arcu. Rd."
+  },
+  {
+    title: "Dui PC",
+    info: "Ap #346-4865 Ipsum. St."
+  },
+  {
+    title: "Viverra Corp.",
+    info: "P.O. Box 426, 9514 Magna. Avenue"
+  },
+  {
+    title: "In Molestie Tortor Corporation",
+    info: "P.O. Box 279, 3083 Phasellus Av."
+  },
+  {
+    title: "Pellentesque Massa Lobortis Foundation",
+    info: "4963 Mollis Ave"
+  },
+  {
+    title: "Congue Turpis Inc.",
+    info: "5889 Pretium St."
+  },
+  {
+    title: "Vivamus Euismod Urna LLC",
+    info: "Ap #551-8305 Integer Ave"
+  },
+  {
+    title: "Sagittis Corporation",
+    info: "391-7307 Cubilia Road"
+  },
+  {
+    title: "Velit In Company",
+    info: "1569 Libero Ave"
+  },
+  {
+    title: "Lorem Eget Associates",
+    info: "P.O. Box 474, 3574 Condimentum. Avenue"
+  },
+  {
+    title: "Dictum Eu Institute",
+    info: "Ap #908-3232 Pellentesque St."
+  },
+  {
+    title: "Tortor Integer Company",
+    info: "8439 Auctor, Street"
+  },
+  {
+    title: "Posuere Cubilia Ltd",
+    info: "Ap #510-7610 Eget Street"
+  },
+  {
+    title: "Aliquet Consulting",
+    info: "P.O. Box 322, 1894 Suspendisse St."
+  },
+  {
+    title: "Fermentum Metus Aenean LLC",
+    info: "226-7093 In Rd."
+  },
+  {
+    title: "Accumsan Interdum Foundation",
+    info: "Ap #431-412 Phasellus Rd."
+  },
+  {
+    title: "Convallis Corporation",
+    info: "674 Est. Rd."
+  },
+  {
+    title: "Molestie Sodales Corporation",
+    info: "923-5854 Duis Avenue"
+  },
+  {
+    title: "Primis Limited",
+    info: "125-5628 Et, St."
+  },
+  {
+    title: "Mauris Corporation",
+    info: "256-7091 Ut Street"
+  },
+  {
+    title: "Nonummy Fusce Corp.",
+    info: "3536 Enim. Rd."
+  },
+  {
+    title: "Urna Corp.",
+    info: "Ap #367-1303 Arcu Road"
+  },
+  {
+    title: "Tincidunt Neque Ltd",
+    info: "Ap #599-8338 Fames St."
+  },
+  {
+    title: "Ligula Eu LLC",
+    info: "5349 Duis Avenue"
+  },
+  {
+    title: "Erat Corporation",
+    info: "P.O. Box 544, 5077 Est Rd."
+  },
+  {
+    title: "Enim LLC",
+    info: "711-1658 Convallis Ave"
+  }
+];
